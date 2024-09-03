4767496e27c99fd097359b4140205121073fa174
Evgeniia <vremendm@gmail.com>
2024-09-03 18:50:26 +0200

    do not sum losses for hpn

--- prototypes_riemann.py
+++ prototypes_riemann.py
@@ -1,5 +1,5 @@
 #
-# Obtain hyperspherical prototypes prior to network training.
+# Obtain hyperspherical prototypes prior to network training with Riemannian optimization.
 #
 # @inproceedings{mettes2016hyperspherical,
 #  title={Hyperspherical Prototype Networks},
@@ -11,7 +11,6 @@
 import argparse
 import os
 import random
-import sys
 import time
 
 import ledoh_torch
@@ -19,34 +18,7 @@ import geoopt
 import numpy as np
 import torch
 import torch.nn.functional as F
-import torch.optim as optim
-from torch import nn
 
-
-#
-# PArse user arguments.
-#
-def parse_args():
-    parser = argparse.ArgumentParser(description="Hyperspherical prototypes")
-    parser.add_argument('-c', dest="classes", default=100, type=int)
-    parser.add_argument('-d', dest="dims", default=100, type=int)
-    parser.add_argument('-l', dest="learning_rate", default=0.01, type=float)
-    parser.add_argument('-m', dest="momentum", default=0.9, type=float)
-    parser.add_argument('-e', dest="epochs", default=10000, type=int)
-    parser.add_argument('-s', dest="seed", default=300, type=int)
-    parser.add_argument('-r', dest="resdir", default="", type=str)
-    parser.add_argument('-w', dest="wtvfile", default="", type=str)
-    parser.add_argument('-n', dest="nn", default=2, type=int)
-    parser.add_argument('-o', dest="optim", default="rsgd", type=str)
-    parser.add_argument('--disp', dest="dispersion", default=None, type=str)
-    parser.add_argument('--dispw', dest="dispersion_weight", default=0, type=float)
-    args = parser.parse_args()
-    return args
-
-
-#
-# Compute the loss related to the prototypes.
-#
 def prototype_loss(prototypes):
     # Dot product of normalized prototypes is cosine similarity.
     product = torch.matmul(prototypes, prototypes.t()) + 1
@@ -54,19 +26,21 @@ def prototype_loss(prototypes):
     product -= 2. * torch.diag(torch.diag(product))
     # Minimize maximum cosine similarity.
     loss = product.max(dim=1)[0]
-    return loss.mean(), product.max()
+    return loss.mean()
 
-
-#
-# Compute the semantic relation loss.
-#
-def prototype_loss_sem(prototypes, triplets):
-    product = torch.matmul(prototypes, prototypes.t()) + 1
-    product -= 2. * torch.diag(torch.diag(product))
-    loss1 = -product[triplets[:, 0], triplets[:, 1]]
-    loss2 = product[triplets[:, 2], triplets[:, 3]]
-    return loss1.mean() + loss2.mean(), product.max()
-
+def parse_args():
+    parser = argparse.ArgumentParser(description="Hyperspherical prototypes")
+    parser.add_argument('-c', dest="classes", default=100, type=int)
+    parser.add_argument('-d', dest="dims", default=100, type=int)
+    parser.add_argument('-l', dest="learning_rate", default=0.01, type=float)
+    parser.add_argument('-e', dest="epochs", default=10000, type=int)
+    parser.add_argument('-s', dest="seed", default=300, type=int)
+    parser.add_argument('-r', dest="resdir", default="", type=str)
+    parser.add_argument('-n', dest="nn", default=2, type=int)
+    parser.add_argument('-o', dest="optim", default="rsgd", type=str)
+    parser.add_argument('--disp', dest="dispersion", default=None, type=str)
+    args = parser.parse_args()
+    return args
 
 #
 # Main entry point of the script.
@@ -86,29 +60,6 @@ if __name__ == "__main__":
 
     print(args)
 
-    # Initialize prototypes and optimizer.
-    if os.path.exists(args.wtvfile):
-        print(f"initialiing prototypes with w2vec file {args.wtvfile}")
-        use_wtv = True
-        wtvv = torch.from_numpy(np.load(args.wtvfile)).float().to(device)
-        wtvv = F.normalize(wtvv, p=2, dim=1)
-        wtvsim = torch.matmul(wtvv, wtvv.t()).float()
-
-        # Precompute triplets.
-        nns, others = [], []
-        for i in range(wtvv.shape[0]):
-            sorder = torch.argsort(wtvsim[i, :], descending=True)
-            nns.append(sorder[:args.nn])
-            others.append(sorder[args.nn:-1])
-        triplets = []
-        for i in range(wtvv.shape[0]):
-            for j in nns[i]:
-                for k in others[i]:
-                    triplets.append([i, j, i, k])
-        triplets = torch.tensor(triplets, device=device, dtype=torch.int)
-    else:
-        use_wtv = False
-
     # Initialize prototypes.
     prototypes = torch.randn(args.classes, args.dims, device=device)
     prototypes = geoopt.ManifoldParameter(F.normalize(prototypes, p=2, dim=1), manifold=geoopt.SphereExact())
@@ -118,34 +69,24 @@ if __name__ == "__main__":
         optimizer = geoopt.optim.RiemannianAdam([prototypes], lr=args.learning_rate, stabilize=1)
     else:
         raise ValueError("Unknown optimizer")
-    # Optimize for separation.
+
+    disp_funcs = {
+    "mmd": ledoh_torch.kernel_dispersion.KernelSphereDispersion(),
+    "lloyd": ledoh_torch.lloyd_dispersion.LloydSphereDispersion(),
+    "sliced": ledoh_torch.sliced_dispersion.SlicedSphereDispersion(),
+    "sliced-ax": ledoh_torch.sliced_dispersion.AxisAlignedSlicedSphereDispersion(),
+    "hpn": prototype_loss
+    }
+    dispersion_fn = disp_funcs[args.dispersion]
     for i in range(args.epochs):
-        # Compute loss.
-        loss1, sep = prototype_loss(prototypes)
-        if use_wtv:
-            loss2, _ = prototype_loss_sem(prototypes, triplets)
-            loss = loss1 + loss2
-        else:
-            loss = loss1
-
-        if args.dispersion=='mmd':
-            disp_loss = ledoh_torch.kernel_dispersion.KernelSphereDispersion().forward(prototypes)
-        elif args.dispersion=='lloyd':
-            disp_loss = ledoh_torch.lloyd_dispersion.LloydSphereDispersion().forward(prototypes)[0]
-        elif args.dispersion=='sliced':
-            disp_loss = ledoh_torch.sliced_dispersion.SlicedSphereDispersion().forward(prototypes, p=None, q=None)
-        else:
-            disp_loss = None
-        # Update.
-
-        if disp_loss is not None:
-            loss += args.dispersion_weight * disp_loss
-
+        loss = dispersion_fn(prototypes)
+        d_min = ledoh_torch.minimum_acos_distance(prototypes.detach().clone())
+        c_var = ledoh_torch.circular_variance(prototypes.detach().clone())
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
         c_time = time.strftime("%d %m %Y %H:%M:%S",time.localtime())
-        print(c_time," ", "%03d/%d: %.4f\r" % (i, args.epochs, sep))
+        print(f"{c_time} Epoch {i} Loss {loss.item()} Dmin {d_min.item()} Cvar {c_var.item()}")
 
     # Store result.
     os.makedirs(args.resdir, exist_ok=True)
